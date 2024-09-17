344cfccd13532c03b39f089ec10ecb3cd4f2c262
Noeliaapama <noelia.apa.92@gmail.com>
2024-09-17 18:13:23 +0200

    analisis_de_texto

---
+++ primer_proyecto.py
@@ -0,0 +1,7 @@
+'''
+primero tienen que ingresar un texto, el que sea con un input
+pedir al usuario 3 letras al azar y devolverá al usuario:
+cuantas veces aparece cada letra
+cuantas palabras hay en total
+informar de 
+'''
